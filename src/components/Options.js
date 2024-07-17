770fd2b282a937ed08618bf280bb20a8c21781d6
slavchokaramfiloski <slavcho.1998@outlook.com>
2024-07-17 15:53:18 +0200

    uploading files

---
+++ src/components/Options.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Option({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
+  console.log(question);
+  return (
+    <div>
+      <div className="options">
+        {question.options.map((option, index) => (
+          <button
+            className={`btn btn-option ${index === answer ? "answer" : ""} ${
+              hasAnswered
+                ? index === question.correctOption
+                  ? "correct"
+                  : "wrong"
+                : ""
+            }`}
+            key={option}
+            disabled={hasAnswered}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
